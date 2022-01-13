7fb5882f53f04167d62aa27a3cb86b138da728f1
Benjamin Jentsch <benjamin.jentsch@gutefrage.net>
2022-01-13 12:55:35 +0100

    Update stylelint to v14.2.0, replace ava with jest

--- rules/custom-property-no-outside-root/index.js
+++ rules/custom-property-no-outside-root/index.js
@@ -9,35 +9,36 @@ const messages = stylelint.utils.ruleMessages(ruleName, {
   rejected: 'Unexpected custom property',
 });
 
-function rule(actual) {
-  return function(root, result) {
-    const validOptions = stylelint.utils.validateOptions(result, ruleName, {actual});
-    if (!validOptions) {
-      return;
-    }
-
-    root.walkRules(rule => {
-      // Ignore rules whose selector is just `:root`
-      if (rule.selector.toLowerCase().trim() === ':root') {
+module.exports = stylelint.createPlugin(
+  ruleName,
+  function(actual) {
+    return (root, result) => {
+      const validOptions = stylelint.utils.validateOptions(result, ruleName, {actual});
+      if (!validOptions) {
         return;
       }
 
-      rule.walkDecls(decl => {
-        if (!isCustomProperty(decl.prop)) {
+      root.walkRules(rule => {
+        // Ignore rules whose selector is just `:root`
+        if (rule.selector.toLowerCase().trim() === ':root') {
           return;
         }
-        stylelint.utils.report({
-          message: messages.rejected,
-          node: decl,
-          result,
-          ruleName,
+
+        rule.walkDecls(decl => {
+          if (!isCustomProperty(decl.prop)) {
+            return;
+          }
+          stylelint.utils.report({
+            message: messages.rejected,
+            node: decl,
+            result,
+            ruleName,
+          });
         });
       });
-    });
-  };
-}
+    };
+  }
+);
 
-rule.ruleName = ruleName;
-rule.messages = messages;
-
-module.exports = rule;
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
